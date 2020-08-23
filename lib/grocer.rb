2fecbe178c4b4db52618725820720d74ecf330af
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-23 21:10:21 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -30,12 +30,10 @@ def consolidate_cart(cart)
   cart.each do |grocery_item|
     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
     if current_item
-      new_cart_index = 0
       new_cart.each do |new_cart_item|
         if new_cart_item[:item] === current_item[:item]
           new_cart_item[:count] += 1
         end
-        new_cart_index += 1
       end
     else
       grocery_item[:count] = 1
